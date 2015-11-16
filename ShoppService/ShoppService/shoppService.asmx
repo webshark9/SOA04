c07e48d5700e56333627ae72372495d7bea9382a
reed_pro <poirier.jordan@gmail.com>
2015-11-16 08:20:09 -0500

    Started web service application
    Hope it works good yo

---
+++ ShoppService/ShoppService/shoppService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="shoppService.asmx.cs" Class="ShoppService.shoppService" %>
---
+++ ShoppService/ShoppService/shoppService.asmx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+
+namespace ShoppService
+{
+    /// <summary>
+    /// Summary description for shoppService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class shoppService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
